318d02a2e67433ced6c35571c9c2a2867f6620c0
Steve White <srwhite@me.com>
2019-03-26 14:06:45 -0400

    BETA: HubConnect Switch with SmartThings Tiles!
    This is a first pass, beta driver with tiles for controlling switch devices on SmartThings.

---
+++ SmartThings/DeviceTypes/HubConnect Switch.groovy
@@ -0,0 +1,147 @@
+/*
+ *	Copyright 2019 Steve White
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *	use this file except in compliance with the License. You may obtain a copy
+ *	of the License at:
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *	WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *	License for the specific language governing permissions and limitations
+ *	under the License.
+ *
+ *
+ */
+metadata 
+{
+	definition(name: "HubConnect Switch", namespace: "shackrat", author: "Steve White", importUrl: "https://raw.githubusercontent.com/HubitatCommunity/HubConnect/master/UniversalDrivers/HubConnect-Switch.groovy")
+	{
+		capability "Switch"
+		capability "Refresh"
+		
+		command "sync"
+	}
+
+	tiles(scale: 2)
+	{
+		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true)
+		{
+			tileAttribute ("device.switch", key: "PRIMARY_CONTROL")
+			{
+				attributeState "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00A0DC", nextState: "turningOff"
+				attributeState "off", label: 'Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "turningOn"
+				attributeState "turningOn", label: 'Turning On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00A0DC", nextState: "turningOff"
+				attributeState "turningOff", label: 'Turning Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "turningOn"
+			}
+		}
+
+		standardTile("indicator", "device.indicatorStatus", width: 2, height: 2, inactiveLabel: false, decoration: "flat")
+		{
+			state "when off", action:"indicator.indicatorWhenOn", icon:"st.indicators.lit-when-off"
+			state "when on", action:"indicator.indicatorNever", icon:"st.indicators.lit-when-on"
+			state "never", action:"indicator.indicatorWhenOff", icon:"st.indicators.never-lit"
+		}
+		standardTile("refresh", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat")
+		{
+			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+
+		main "switch"
+		details(["switch","refresh"])
+	}
+}
+
+
+/*
+	installed
+    
+	Doesn't do much other than call initialize().
+*/
+def installed()
+{
+	initialize()
+}
+
+
+/*
+	updated
+    
+	Doesn't do much other than call initialize().
+*/
+def updated()
+{
+	initialize()
+}
+
+
+/*
+	initialize
+    
+	Doesn't do much other than call refresh().
+*/
+def initialize()
+{
+	refresh()
+}
+
+
+/*
+	parse
+    
+	In a virtual world this should never be called.
+*/
+def parse(String description)
+{
+	log.trace "Msg: Description is $description"
+}
+
+
+/*
+	on
+    
+	Turns the device on.
+*/
+def on()
+{
+	// The server will update on/off status
+	parent.sendDeviceEvent(device.deviceNetworkId, "on")
+}
+
+
+/*
+	off
+    
+	Turns the device off.
+*/
+def off()
+{
+	// The server will update on/off status
+	parent.sendDeviceEvent(device.deviceNetworkId, "off")
+}
+
+
+/*
+	refresh
+    
+	Refreshes the device by requesting an update from the client hub.
+*/
+def refresh()
+{
+	// The server will update status
+	parent.sendDeviceEvent(device.deviceNetworkId, "refresh")
+}
+
+
+/*
+	sync
+    
+	Synchronizes the device details with the parent.
+*/
+def sync()
+{
+	// The server will respond with updated status and details
+	parent.syncDevice(device.deviceNetworkId, "switch")
+}
