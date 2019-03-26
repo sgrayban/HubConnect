74da55013f5b1dc012406c9b08914b08464436fc
Steve White <srwhite@me.com>
2019-03-26 14:24:07 -0400

    BETA: Pocket Socket SmartThings DeviceType with Tiles
    Adding the pocket socket device type since it shares a common codebase with the switch type.

--- SmartThings/DeviceTypes/HubConnect Pocket Socket.groovy
+++ SmartThings/DeviceTypes/HubConnect Pocket Socket.groovy
@@ -17,29 +17,34 @@
  */
 metadata 
 {
-	definition(name: "HubConnect Switch", namespace: "shackrat", author: "Steve White", importUrl: "https://raw.githubusercontent.com/HubitatCommunity/HubConnect/master/UniversalDrivers/HubConnect-Switch.groovy")
+	definition(name: "HubConnect Pocket Socket", namespace: "shackrat", author: "Steve White", importUrl: "https://raw.githubusercontent.com/HubitatCommunity/HubConnect/master/UniversalDrivers/HubConnect-Pocket-Socket.groovy")
 	{
 		capability "Switch"
+		capability "Power Meter"
 		capability "Refresh"
-		
+
 		command "sync"
 	}
 
 	tiles(scale: 2)
 	{
-		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true)
+		multiAttributeTile(name: "switch", type: "lighting", width: 6, height: 4, canChangeIcon: true)
 		{
-			tileAttribute ("device.switch", key: "PRIMARY_CONTROL")
+			tileAttribute("device.switch", key: "PRIMARY_CONTROL")
 			{
 				attributeState "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00A0DC", nextState: "turningOff"
 				attributeState "off", label: 'Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "turningOn"
 				attributeState "turningOn", label: 'Turning On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00A0DC", nextState: "turningOff"
 				attributeState "turningOff", label: 'Turning Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "turningOn"
 			}
+			tileAttribute("power", key: "SECONDARY_CONTROL")
+			{
+				attributeState "power", label: '${currentValue} W'
+			}
 		}
-		standardTile("refresh", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat")
+		standardTile("refresh", "device.power", inactiveLabel: false, decoration: "flat", width: 2, height: 2)
 		{
-			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
+			state "default", label: '', action: "refresh.refresh", icon: "st.secondary.refresh"
 		}
 		standardTile("sync", "sync", inactiveLabel: false, decoration: "flat", width: 2, height: 2)
 		{
@@ -140,5 +145,5 @@ def refresh()
 def sync()
 {
 	// The server will respond with updated status and details
-	parent.syncDevice(device.deviceNetworkId, "switch")
+	parent.syncDevice(device.deviceNetworkId, "pocketsocket")
 }
